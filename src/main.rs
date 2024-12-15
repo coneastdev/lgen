513453edc85bdf49e72464d6667ce9d2c6b7933a
coneastdev <124074325+coneastdev@users.noreply.github.com>
2024-12-15 11:16:08 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -118,7 +118,7 @@ pub fn genlicence(lic: &str) {
                       GNU GENERAL PUBLIC LICENSE
                         Version 3, 29 June 2007
 
-    Copyright (C) <date> <copyright holder>
+    Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
     Everyone is permitted to copy and distribute verbatim copies
     of this license document, but changing it is not allowed.
 
